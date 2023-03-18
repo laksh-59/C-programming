2325da233e5098736bfe9752882dd4a709202c12
laksh-59 <lakshsodhai59@gmail.com>
2023-03-18 15:22:12 +0530

    first commit

---
+++ q1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{ int a=2;
+char b='d';
+float c=3.0;
+printf("%d %c %f",a,b,c);
+
+
+
+return 0;
+
+
+}
---
+++ q2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{int a=2;
+int b=4,c;
+c=a;
+a=b;
+b=c;
+
+printf("%d %d",a,b,c);
+return 0;
+
+
+
+
+}
